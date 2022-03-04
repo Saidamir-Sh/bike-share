d52eb489ab08346ba7ce5f2b096d7e7e224a78b0
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-03-04 16:55:56 +0100

    extra features: dark/light mode

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,21 +3,28 @@ import '../styles/Dashboard.css'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import { Switch } from '@mui/material';
 
 function Dashboard({latitude, longitude}) {
 
   const [isActive, setIsActive] = useState(false)
+  const [isLightMode, setIsLightMode] = useState(true)
 
-  const handleToggle = () => {
+  const toggleMode = () => {
+    setIsLightMode(!isLightMode)
+  }
+
+  const handleSideBar = () => {
     setIsActive(!isActive)
   }
-  console.log(isActive)
 
   return (
         <div className={isActive ? 'dashboard' : 'dashboard inactive'}>
+           <Switch  onChange={toggleMode}/>
 
-          <div onClick={handleToggle} className='dashboard-arrow d-flex align-items-center justify-content-center'>
-            <ArrowRightIcon />
+          <div onClick={handleSideBar} className='dashboard-arrow d-flex align-items-center justify-content-center'>
+            {isActive ? <ArrowRightIcon /> : <ArrowLeftIcon/>}
           </div>
         </div>
   )
