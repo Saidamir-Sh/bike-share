414e41e55789c3fb2519eef7745dd8c5105601ad
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-03-08 16:02:11 +0100

    input filtering function

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Dashboard.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleMode } from '../redux/action'
@@ -11,8 +11,13 @@ const Dashboard = ({station}) => {
 
   const dispatch = useDispatch()
 
+
+  const [searchQuery, setSearchQuery] = useState('')
+
   const isLightMode = useSelector((state) => state.isLightMode)
-  //console.log(station)
+  const networks = useSelector((state) => state.bikeNetworks.networks)
+  console.log(networks)
+
 
   const [isActive, setIsActive] = useState(false)
   
@@ -24,9 +29,25 @@ const Dashboard = ({station}) => {
         <div className={isActive ? 'dashboard ' : 'dashboard inactive dashboard-dark'}>
            <Switch  onChange={() => {dispatch(toggleMode())}}/>
            <Form>
-            <Form.Group controlId="formBasicText">
-              <Form.Control className='search-input mx-auto mt-3' type="text" placeholder="Search for other cities..." />
+            <Form.Group className='d-flex align-items-center position-relative' controlId="formBasicText">
+              <Form.Control 
+              onChange={(e) => setSearchQuery(e.target.value)}
+              value={searchQuery}
+              className='search-input mx-auto mt-3 shadow-none' 
+              type="text" 
+              placeholder="Search for other cities..." />
+              <i class="bi bi-search"></i>
             </Form.Group>
+            {
+              networks.filter((network) => {
+                if(!searchQuery) return false
+                if(network.location.city.toLowerCase().includes(searchQuery)) return true
+              }).map((network) => (
+                <div>
+                  <p>{network.location.city}</p>
+                </div>
+              ))
+            }
           </Form>
       
           <div onClick={handleSideBar} className='dashboard-arrow-dark dashboard-arrow d-flex align-items-center justify-content-center'>
